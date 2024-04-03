960cdd3e574dcf9a4588a563efca0916372d64d0
raymond li <raymond_li@manulife.com>
2024-04-03 17:58:03 +0800

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Raymond Li （李英杰） on 2024/4/3.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
